89056f4b421229943ca784467ab0a55fa5c55787
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-01-20 21:51:41 +0530

    click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const clicking = ()=>{
+        console.log("Hello i m preetam")
+    }
+    const click = (name)=>{
+        console.log("hello i m "+name + " again")
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={clicking}>click me</button>
+            <button onClick={()=>{
+                click("preetam")
+            }}>click me</button>
         </div>
      );
 }
