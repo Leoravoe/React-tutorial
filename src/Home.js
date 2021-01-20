0b19b349943d51c2d20b78a5a21258d5d023fb19
Preetam Sarkar <sarkarpreetam12@gmail.com>
2021-01-21 00:31:58 +0530

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,12 @@
+import {useState} from 'react'
+
+
 const Home = () => {
+    const [name , setName] = useState('preetam')
+    const [age , setAge] = useState(21)
     const clicking = ()=>{
-        console.log("Hello i m preetam")
+        setName('Preetam Sarkar')
+        setAge(22)
     }
     const click = (name)=>{
         console.log("hello i m "+name + " again")
@@ -8,6 +14,7 @@ const Home = () => {
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={clicking}>click me</button>
             <button onClick={()=>{
                 click("preetam")
